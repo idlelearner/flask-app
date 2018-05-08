3838fb011a23030295ccb3dee095d18a6f2c9459
Dhass <t.bhags21@gmail.com>
2018-05-07 20:06:47 -0700

    User profile

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from hashlib import md5
 from app import login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,6 +10,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
     # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
     posts = db.relationship('Post', backref='author', lazy='dynamic')
@@ -21,6 +24,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Post(db.Model):
